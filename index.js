b0bcf9f7d676a60930b58472b31314b17457f94f
Vignesh Mudhiraj <vigguvignesh92@gmail.com>
2025-08-15 22:55:16 +0530

    feat: Add support for new problems

--- index.js
+++ index.js
@@ -71,10 +71,10 @@ async function scrapeAndStore() {
     console.log("Starting the full scraping process...");
 
     let contestUrls = [];
-    for (let i = 394; i <= 443; i++) {
+    for (let i = 444; i <= 462; i++) {
         contestUrls.push({ url: `https://leetcode.com/contest/weekly-contest-${i}/`, name: `Weekly Contest ${i}`, type: "Weekly" });
     }
-    for (let i = 104; i <= 153; i++) {
+    for (let i = 154; i <= 162; i++) {
         contestUrls.push({ url: `https://leetcode.com/contest/biweekly-contest-${i}/`, name: `Biweekly Contest ${i}`, type: "Biweekly" });
     }
 
